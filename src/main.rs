b229255b1c50002ef641b31ef4d0f0a4d938dc24
w.sepo <w.sepo@commonfactor.tech>
2025-11-01 14:32:56 +0100

    FEATURE: add command line handler

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+fn handle_command() {
+    let mut command = String::new();
+    io::stdin().read_line(&mut command).unwrap();
+    let command = command.trim();
+
+    match command {
+        _ => {
+            println!("{}: Command not found", command);
+        }
+    }
+}
+
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
+    handle_command();
 }
