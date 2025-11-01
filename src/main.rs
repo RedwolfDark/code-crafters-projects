5fd6ffa3be8d67e91c75e37fb6fefabae1f07f4a
w.sepo <w.sepo@commonfactor.tech>
2025-11-01 17:15:03 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,7 @@ fn handle_command_not_found(command: &str) {
     println!("{}: command not found", command);
 }
 
-fn handle_command(command: String) {
-    let mut command_and_args = command.split_whitespace();
-    let command = command_and_args.next().unwrap();
-    let args: Vec<&str> = command_and_args.collect();
-
+fn handle_command(command: &str, args: Vec<&str>) {
     let built_in_commands = vec!["echo", "cd", "exit"];
 
     // handle type command
@@ -29,22 +25,28 @@ fn handle_command(command: String) {
         println!("{}", args.join(" "));
         return;
     }
+
     handle_command_not_found(command);
 }
 
 fn handle_inputs() {
+    const EXIT_COMMAND: &str = "exit";
     let mut command = String::new();
+
     io::stdin().read_line(&mut command).unwrap();
+
     let command = command.trim().to_string();
 
-    const EXIT_COMMAND: &str = "exit";
+    let mut command_and_args = command.split_whitespace();
+    let command = command_and_args.next().unwrap();
+    let args: Vec<&str> = command_and_args.collect();
 
     match command.contains(EXIT_COMMAND) {
         true => {
             std::process::exit(0);
         }
         false => {
-            handle_command(command);
+            handle_command(command, args);
         }
     }
 }
