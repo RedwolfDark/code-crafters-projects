fb70146152c52f2f71f35c5c80fd145f361ee4ad
w.sepo <w.sepo@commonfactor.tech>
2025-11-29 01:22:35 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -3,41 +3,61 @@ use std::fs::{self};
 use std::io::{self, Write};
 use std::os::unix::fs::PermissionsExt;
 use std::path::Path;
+use std::process::Command;
 
 const EXIT_COMMAND: &str = "exit";
 const EXECUTABLE_PERMISSION: u32 = 0o111;
 const BUILTIN_COMMANDS: [&str; 4] = ["echo", "cd", "exit", "type"];
 
-fn handle_command_not_found(command: &str, context: Option<&str>) {
+fn _handle_command_not_found(command: &str, context: Option<&str>) {
     println!("{}: {}not found", command, context.unwrap_or(""));
 }
 
-fn handle_command(command: &str, args: Vec<&str>) {
-    // handle type command
-    if command == "type" {
-        for arg in &args {
-            if BUILTIN_COMMANDS.contains(&arg) {
-                println!("{} is a shell builtin", *arg);
-            } else if check_command_in_path(&arg).0 {
-                println!("{} is {}", *arg, check_command_in_path(&arg).1);
-            } else {
-                handle_command_not_found(*arg, None);
-            }
-        }
-
-        return;
+fn _handle_command(command: &str, args: Vec<&str>) {
+    match command {
+        "type" => _type_command(args),
+        "echo" => _echo_command(args),
+        _ => _execute_command(command, args),
     }
-
-    // handle built-in commands
-    if command == "echo" {
-        println!("{}", args.join(" "));
-        return;
-    }
-
-    handle_command_not_found(command, Some("command "));
 }
 
-fn handle_inputs() {
+fn _execute_command(_command: &str, _args: Vec<&str>) {
+    let (is_command_in_path, command_executable) = _check_command_in_path(_command);
+
+    if is_command_in_path {
+        let mut command = Command::new(command_executable);
+        let command_with_args = command.args(&_args);
+
+        if let Ok(mut command_result) = command_with_args.spawn() {
+            command_result.wait().unwrap_or_else(|_| {
+                _handle_command_not_found(&_command, Some("command "));
+                std::process::exit(1);
+            });
+        } else {
+            _handle_command_not_found(_command, Some("command "));
+        }
+    } else {
+        _handle_command_not_found(_command, Some("command "));
+    }
+}
+
+fn _type_command(args: Vec<&str>) {
+    for arg in &args {
+        if BUILTIN_COMMANDS.contains(&arg) {
+            println!("{} is a shell builtin", *arg);
+        } else if _check_command_in_path(&arg).0 {
+            println!("{} is {}", *arg, _check_command_in_path(&arg).1);
+        } else {
+            _handle_command_not_found(*arg, None);
+        }
+    }
+}
+
+fn _echo_command(args: Vec<&str>) {
+    println!("{}", args.join(" "));
+}
+
+fn _handle_inputs() {
     let mut command = String::new();
 
     io::stdin().read_line(&mut command).unwrap();
@@ -53,20 +73,20 @@ fn handle_inputs() {
             std::process::exit(0);
         }
         false => {
-            handle_command(command, args);
+            _handle_command(command, args);
         }
     }
 }
 
-fn display_prompt(promt: String) {
+fn _display_prompt(promt: String) {
     print!("{} ", promt);
     io::stdout().flush().unwrap();
 }
 
-fn check_command_in_path(command: &str) -> (bool, String) {
+fn _check_command_in_path(command: &str) -> (bool, String) {
     if let Ok(path) = var("PATH") {
         for p in path.split(':') {
-            let (found, fullpath) = check_command_in_folder(command, p);
+            let (found, fullpath) = _check_command_in_folder(command, p);
             if found {
                 return (true, fullpath);
             }
@@ -76,7 +96,7 @@ fn check_command_in_path(command: &str) -> (bool, String) {
     (false, String::new())
 }
 
-fn check_command_in_folder(command: &str, folder: &str) -> (bool, String) {
+fn _check_command_in_folder(command: &str, folder: &str) -> (bool, String) {
     let path = Path::new(folder).join(command);
 
     if !path.exists() {
@@ -99,7 +119,7 @@ fn check_command_in_folder(command: &str, folder: &str) -> (bool, String) {
 fn main() {
     loop {
         let prompt = String::from("$");
-        display_prompt(prompt);
-        handle_inputs();
+        _display_prompt(prompt);
+        _handle_inputs();
     }
 }
